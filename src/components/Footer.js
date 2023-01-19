f8ce8a8f9fd3ad5c6ae2c5a7ad13e451f0b9e0ec
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2023-01-19 16:04:18 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
+import React from "react";
+const Footer = (props) => {
+    return (
+        <div id="footer">
 
+            <footer id={props.id}>{props.content}</footer>
+        </div>
+    );
+}
+
+export default Footer;
