e8f1580286642ce24f7ad633747e5769c02f038d
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2023-01-19 16:05:14 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Main = (props) => {
+    return (
+        <div id={props.id}>
+            <main id="container">This is the main</main>
+        </div>
+    );
+}
+
+export default Main;
